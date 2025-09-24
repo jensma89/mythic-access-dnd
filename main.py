abaa3baa92bb57687c0572d76d6638175e24377c
Jens M. <jensmayer89@gmail.com>
2025-09-24 13:51:13 +0200

    Change the on_event to lifespan

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ main.py
 Webserver entry
 """
 from fastapi import FastAPI
+from dependencies import create_db_and_tables
+from contextlib import asynccontextmanager
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Create the tables and start and stop the DB session"""
+    create_db_and_tables() # Create the tables
+    print("Server started!")
+    yield
+    print("Server stopped!")
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello FastAPI"}
+app = FastAPI(lifespan=lifespan)
--- models/db_models/models.py
+++ models/db_models/models.py
@@ -4,8 +4,16 @@ models.py
 Table models for DB.
 """
 from sqlmodel import Field, SQLModel, select
+from pydantic import EmailStr
+from datetime import datetime, timezone
 
 
 
 class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    user_name: str = Field(unique=True)
+    email: EmailStr = Field(unique=True)
+    hashed_password: str
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime | None = None
 
