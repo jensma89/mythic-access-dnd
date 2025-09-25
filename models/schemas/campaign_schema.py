854b16968596429bf8746a84272a59dbf18bae79
Jens M. <jensmayer89@gmail.com>
2025-09-25 12:23:03 +0200

    Add campaign pydantic models

--- models/schemas/campaign_schema.py
+++ models/schemas/campaign_schema.py
@@ -6,6 +6,7 @@ Request/response schemas for campaigns.
 from sqlmodel import SQLModel
 
 
+
 class CampaignBase(SQLModel):
     """Base Campaign model that shares common definitions"""
     title: str
@@ -14,26 +15,19 @@ class CampaignBase(SQLModel):
     max_classes: int
 
 
-class CampaignCreate(SQLModel):
+class CampaignCreate(CampaignBase):
+    """Fields to create a campaign"""
+    created_by: int
 
 
-
-
-
-class Campaign(SQLModel, table=True):
-    """Model to create a campaign table."""
-    __table_args__ = (
-        UniqueConstraint("title",
-                         "created_by",
-                         name="uq_user_title"),
-    )
-    id: int | None = Field(default=None, primary_key=True)
-    title: str
+class CampaignUpdate(SQLModel):
+    """Fields to update a campaign"""
+    title: str | None = None
     genre: str | None = None
-    description: str | None = Field(default=None, description="Story of the campaign")
-    max_classes: int
-    created_by: int = Field(foreign_key="user.id")
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    description: str | None = None
 
-    # ORM link to User
-    creator: Optional[User] = Relationship(back_populates="campaigns")
+
+class CampaignPublic(CampaignBase):
+    """Model to respond public data"""
+    created_by: int
+    created_at: str
