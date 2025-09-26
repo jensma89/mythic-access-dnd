7c9f976110549408dc896b0f30ee8bfa980b8015
Jens M. <jensmayer89@gmail.com>
2025-09-26 14:09:19 +0200

    Add sql user repository

--- repositories/sql_user_repository.py
+++ repositories/sql_user_repository.py
@@ -3,3 +3,62 @@ sql_user_repository.py
 
 Concrete SQL model for user management.
 """
+from typing import List, Optional
+from sqlmodel import Session, select
+from models.db_models.table_models import User
+from models.schemas.user_schema import UserCreate, UserUpdate, UserPublic
+from user_repository import UserRepository
+
+
+
+class SqlAlchemyUserRepository(UserRepository):
+    def __init__(self, session: Session):
+        self.session = session
+
+
+    def get_by_id(self, user_id: int) -> Optional[UserPublic]:
+        db_user = self.session.get(User, user_id)
+        if db_user:
+            return UserPublic.model_validate(db_user)
+        return None
+
+
+    def list_all(self,
+                 offset: int = 0,
+                 limit: int = 100) -> List[UserPublic]:
+        users = self.session.exec(
+            select(User)
+            .offset(offset)
+            .limit(limit)).all()
+        return [UserPublic.model_validate(u) for u in users]
+
+
+    def add(self, user: UserCreate) -> UserPublic:
+        db_user = User(**user.model_dump())
+        self.session.add(db_user)
+        self.session.commit()
+        self.session.refresh(db_user)
+        return UserPublic.model_validate(db_user)
+
+
+    def update(self,
+               user_id: int,
+               user: UserUpdate) -> Optional[UserPublic]:
+        db_user = self.session.get(User, user_id)
+        if not db_user:
+            return None
+        for key, value in user.model_dump(exclude_unset=True).items():
+            setattr(db_user, key, value)
+        self.session.add(db_user)
+        self.session.commit()
+        self.session.refresh(db_user)
+        return UserPublic.model_validate(db_user)
+
+
+    def delete(self, user_id: int) -> bool:
+        db_user = self.session.get(User, user_id)
+        if not db_user:
+            return False
+        self.session.delete(db_user)
+        self.session.commit()
+        return True
--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -10,26 +10,33 @@ from models.schemas.user_schema import *
 
 
 class UserRepository(ABC):
+    """This class defines the management methods for user"""
+
     @abstractmethod
     def get_by_id(self, user_id: int) -> Optional[UserPublic]:
+        """Get user by id method"""
         pass
 
 
     @abstractmethod
     def list_all(self, offset: int = 0, limit: int = 100) -> List[UserPublic]:
+        """Show all users method"""
         pass
 
 
     @abstractmethod
     def add(self, user: UserCreate) -> UserPublic:
+        """Add new user method"""
         pass
 
 
     @abstractmethod
     def update(self, user_id: int, user: UserUpdate) -> Optional[UserPublic]:
+        """Change user data method"""
         pass
 
 
     @abstractmethod
     def delete(self, user_id: int) -> bool:
+        """Remove a user method"""
         pass
