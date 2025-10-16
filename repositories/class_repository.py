11a08b184c4eb41a010d5f9b0b14d89a17937681
Jens M. <jensmayer89@gmail.com>
2025-10-16 17:29:24 +0200

    Add class repository

---
+++ repositories/class_repository.py
@@ -0,0 +1,48 @@
+"""
+class_repository.py
+
+Defined methods for class management.
+"""
+from fastapi import Query
+from abc import ABC, abstractmethod
+from typing import Annotated, List, Optional
+from models.schemas.class_schema import *
+
+
+
+class ClassRepository(ABC):
+    """This class defines the management methods for classes."""
+
+    @abstractmethod
+    def get_by_id(self, class_id: int) -> Optional[ClassPublic]:
+        """Method to get class by ID."""
+        pass
+
+
+    @abstractmethod
+    def list_all(self,
+                 offset: Annotated[int, Query(ge=0)] = 0,
+                 limit: Annotated[int, Query(le=100)] = 100
+                 ) -> List[ClassPublic]:
+        """Show all classes method."""
+        pass
+
+
+    @abstractmethod
+    def add(self, new_class: ClassCreate) -> ClassPublic:
+        """Add new class method."""
+        pass
+
+
+    @abstractmethod
+    def update(self,
+               class_id: int,
+               update_class: ClassUpdate) -> Optional[ClasPublic]:
+        """Model to change data."""
+        pass
+
+
+    @abstractmethod
+    def delete(self, class_id: int) -> bool:
+        """Delete class method."""
+        pass
