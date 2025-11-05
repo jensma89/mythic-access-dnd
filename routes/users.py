45d19b3ab31f5a7e4e20952ba6d346c74ccfd143
Jens M. <jensmayer89@gmail.com>
2025-11-05 14:24:17 +0100

    Add auth middleware for user

--- routes/users.py
+++ routes/users.py
@@ -4,15 +4,19 @@ users.py
 The API endpoints for users.
 """
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException,Path ,Query
 from dependencies import Pagination, SessionDep, UserQueryParams
-from models.schemas.user_schema import *
+from models.db_models.table_models import User
+from models.schemas.user_schema import (UserCreate,
+                                        UserUpdate,
+                                        UserPublic)
 from repositories.sql_user_repository import SqlAlchemyUserRepository
 from services.user_service import UserService
 from repositories.sql_campaign_repository import SqlAlchemyCampaignRepository
 from repositories.sql_class_repository import SqlAlchemyClassRepository
 from repositories.sql_diceset_repository import SqlAlchemyDiceSetRepository
 from repositories.sql_dicelog_repository import SqlAlchemyDiceLogRepository
+from auth.auth import get_current_user
 
 
 
@@ -38,7 +42,8 @@ def get_user_service(session: SessionDep) \
 @router.get("/users/{user_id}",
             response_model=UserPublic)
 def read_user(
-        user_id: int,
+        user_id: int = Path(..., description="The ID of the user to retrieve"),
+        current_user: User = Depends(get_current_user),
         service: UserService = Depends(get_user_service)):
     """Endpoint to get a single user."""
     user = service.get_user(user_id)
@@ -52,6 +57,7 @@ def read_user(
 @router.get("/users/",
             response_model=List[UserPublic])
 def read_users(
+        current_user: User = Depends(get_current_user),
         pagination: Pagination = Depends(),
         filters: UserQueryParams = Depends(),
         service: UserService = Depends(get_user_service)):
@@ -65,8 +71,9 @@ def read_users(
 @router.patch("/users/{user_id}",
             response_model=UserPublic)
 def update_user(
-        user_id: int,
         user: UserUpdate,
+        user_id: int = Path(..., description="The ID of the user to update."),
+        current_user: User = Depends(get_current_user),
         service: UserService = Depends(get_user_service)):
     """Endpoint to change user data."""
     updated = service.update_user(user_id, user)
@@ -80,7 +87,8 @@ def update_user(
 @router.delete("/users/{user_id}",
                response_model=UserPublic)
 def delete_user(
-        user_id: int,
+        user_id: int = Path(..., description="The ID of the user to delete."),
+        current_user: User = Depends(get_current_user),
         service: UserService = Depends(get_user_service)):
     """Endpoint to delete a user by id."""
     deleted = service.delete_user(user_id)
