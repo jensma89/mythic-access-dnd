f6eebc15fcc0f89f5fb3624f3c090f8bb5ac2aa3
Jens M. <jensmayer89@gmail.com>
2025-11-12 22:20:13 +0100

    Create a start endpoint

--- main.py
+++ main.py
@@ -28,6 +28,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan, title="Mythic Access DnD")
 
+
+@app.get("/")
+def welcome():
+    """Start endpoint."""
+    return {"message": "Welcome to the Mythic Access DnD API"}
+
+
 # Attach limiter to app state
 app.state.limiter = limiter
 
@@ -54,13 +61,13 @@ app.add_middleware(
 
 
 # Link to routes
-app.include_router(users.router, prefix="/api")
-app.include_router(campaigns.router, prefix="/api")
-app.include_router(classes.router, prefix="/api")
-app.include_router(dices.router, prefix="/api")
-app.include_router(dicesets.router, prefix="/api")
-app.include_router(dicelogs.router, prefix="/api")
-app.include_router(auth_routes.router, prefix="/api")
+app.include_router(users.router)
+app.include_router(campaigns.router)
+app.include_router(classes.router)
+app.include_router(dices.router)
+app.include_router(dicesets.router)
+app.include_router(dicelogs.router)
+app.include_router(auth_routes.router)
 
 
 @app.get("/healthz")
